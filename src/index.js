4294629bba3cfab06a666b479b4c4d2a755e8072
Alexandre Carvalho <alex380bb@gmail.com>
2024-07-03 10:34:40 -0300

    linter

--- src/index.js
+++ src/index.js
@@ -1,50 +1,44 @@
 const { books } = require('../data/library');
 
 // Requisito 1
-const fantasyOrScienceFiction = () => {
-  return books.filter((book) => 
-    book.genre === 'Ficção Científica' || book.genre === 'Fantasia'
-  ); 
-};
-
+const fantasyOrScienceFiction = () =>
+  books.filter((book) =>
+    book.genre === 'Ficção Científica' || book.genre === 'Fantasia');
 
 const oldBooksOrdered = (year) => {
-  const sixtyYearsDiff = year -60;
-  
+  const sixtyYearsDiff = year - 60;
+
   return books
-  .filter((book) => book.releaseYear <= sixtyYearsDiff)
-  .sort((a,b) => a.releaseYear - b.releaseYear);
+    .filter((book) => book.releaseYear <= sixtyYearsDiff)
+    .sort((a, b) => a.releaseYear - b.releaseYear);
 };
 
-const booksByAuthorBirthYear = (birthYear) => {
-  return books
-    .filter(book => book.author.birthYear === birthYear)
-    .map(book => book.name);
-};
+const booksByAuthorBirthYear = (birthYear) =>
+  books
+    .filter((book) => book.author.birthYear === birthYear)
+    .map((book) => book.name);
 
-const fantasyOrScienceFictionAuthors = () => {
-  return books
-  .filter(book => book.genre === 'Ficção Científica' || book.genre === 'Fantasia')
-  .map(book => book.author.name)
-  .sort();
-};
+const fantasyOrScienceFictionAuthors = () =>
+  books
+    .filter((book) => book.genre === 'Ficção Científica' || book.genre === 'Fantasia')
+    .map((book) => book.author.name)
+    .sort();
 
 const oldBooks = (year) => {
   const moreSixtyYears = year - 60;
 
   return books
-    .filter(book => book.releaseYear <= moreSixtyYears)
-    .map(book => book.name);
+    .filter((book) => book.releaseYear <= moreSixtyYears)
+    .map((book) => book.name);
 };
 
 function authorWith3DotsOnName() {
-  return books.find(book => {
+  return books.find((book) => {
     const authorName = book.author.name;
-    const initials = authorName.split(' ').filter(word => word.endsWith('.'));
+    const initials = authorName.split(' ').filter((word) => word.endsWith('.'));
     return initials.length >= 3;
   }).name;
 }
-
 
 module.exports = {
   fantasyOrScienceFiction,
